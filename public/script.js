f407a16ed19b14d88b08db53549710f7d0e4c8c5
Hikkos <noeparedes027@gmail.com>
2023-01-24 17:22:14 -0600

    Added JS

--- public/script.js
+++ public/script.js
@@ -1,31 +1,38 @@
-async function postData(url = '',) {
-    const response = await fetch(url, {
-        method: 'GET', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-          'Content-Type': 'application/json',
-          'x-token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6MSwiaWF0IjoxNjc0NDk4ODMyLCJleHAiOjE2NzQ1MTMyMzJ9.q12Zhs3EFOwEsu2GQntPG94AAiX8FpXpaU2S1tpXpSo'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    
-      });
-      return response.json(); // parses JSON response into native JavaScript objects
-    }
-    const img = []
-    postData('http://localhost:8080/api/Transporte')
-      .then((data) => {
-        console.log(data.servicios[0]);
-        ImgIds = data.servicios[0].ImgIds
-        ImgIds.map(i => {
-          const img = document.createElement('img')
-            img.style= 'height: 200px'
-            img.src = `http://localhost:8080/api/uploads/servicios/${i}`
-            document.body.appendChild(img)
-    
-        })
-      });
-    
+async function postData(url = "") {
+	const response = await fetch(url, {
+		method: "GET", // *GET, POST, PUT, DELETE, etc.
+		mode: "cors", // no-cors, *cors, same-origin
+		cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+		credentials: "same-origin", // include, *same-origin, omit
+		headers: {
+			"Content-Type": "application/json",
+			"x-token":
+				"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6MSwiaWF0IjoxNjc0NTgwMDc1LCJleHAiOjE2NzQ1OTQ0NzV9.JXWiTUVMAEqKc97maiwoGs44EFelbi9AHwg0DHsPvhk",
+			// 'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		redirect: "follow", // manual, *follow, error
+		referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+	});
+	return response.json(); // parses JSON response into native JavaScript objects
+}
+const img = [];
+postData("http://localhost:8080/api/servicios").then((data) => {
+	const { servicios } = data
+	console.log(servicios);
+	const imgPhats = servicios[0].ImgIds
+	console.log(servicios[0].Nombre	)
+	console.log(imgPhats)	
+	imgPhats.map(pt =>{
+		
+		const p = document.createElement('p')
+		p.innerText = pt
+		document.body.appendChild(p) 
+		const img = document.createElement('img')
+		img.style = 'height:100px'
+		const src = `http://localhost:8080/api/uploads/${servicios[0].Nombre}/${pt}`
+		img.src = src
+		console.log(src)
+		document.body.appendChild(img)
+	})
+	
+})
