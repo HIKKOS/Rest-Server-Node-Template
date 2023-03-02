9549c8c60428885c627c5b708c4688924eb780c0
HIKKO <noeparedes027@gmail.com>
2023-03-02 17:32:03 -0600

    Busqueda

--- controllers/busqueda.js
+++ controllers/busqueda.js
@@ -6,23 +6,101 @@ const { all } = require("../routes/auth");
 const { getImgIdsFromService } = require("../helpers/obenterIdImagenes");
 const prisma = new PrismaClient();
 
-const busquedaGet = async (req = request, res = response)=> {
-	const {dataFor = ""} = req.query
-	if(dataFor.toUpperCase() === "WEB"){
-		busquedaWeb(req,res)
+const busquedaServicios = async (req = request, res = response) => {
+	const { dataFor = "" } = req.query;
+	if (dataFor.toUpperCase() === "WEB") {
+		busquedaWeb(req, res);
 	} else {
-		busquedaMobile(req,res)
+		busquedaMobile(req, res);
 	}
-}
+};
+const busquedaAlumnos = async (req = request, res = response) => {
+	const { alumno:query = "" } = req.query;
+	const { page, limit } = req.query;
+	const { skip, limite } = await evaluarPagina(page, limit);
+	const alumnos = await prisma.alumno.findMany({
+		where: {
+			OR: [
+				{
+					PrimerNombre: {
+						startsWith: query,
+					},
+				},
+				{
+					SegundoNombre: {
+						startsWith: query,
+					},
+				},
+				{
+					ApellidoPaterno: {
+						startsWith: query,
+					},
+				},
+				{
+					ApellidoMaterno: {
+						startsWith: query,
+					},
+				},
+			],
+		},
+		skip,
+		take: limite,
+	});
+	return res.json({
+		total: alumnos.length,
+		alumnos,
+	});
+};
+const busquedaTutores = async (req = request, res = response) => {
+	const { tutor:query = "" } = req.query;
+	const { page, limit } = req.query;
+	const { skip, limite } = await evaluarPagina(page, limit);
+	let tutores = await prisma.tutor.findMany({
+		where: {
+			OR: [
+				{
+					PrimerNombre: {
+						startsWith: query,
+					},
+				},
+				{
+					SegundoNombre: {
+						startsWith: query,
+					},
+				},
+				{
+					ApellidoPaterno: {
+						startsWith: query,
+					},
+				},
+				{
+					ApellidoMaterno: {
+						startsWith: query,
+					},
+				},
+			],
+		},
+		skip,
+		take: limite,
+	});
+	tutores = tutores.map((t) => {
+		const { PasswordTutor, CreatedAt, MetodoPago, Activo, ...resto } = t;
+		return resto;
+	});
+	return res.json({
+		total: tutores.length,
+		tutores: tutores.length === 0 ? 'sin resultados' :tutores ,
+	});
+};
 const busquedaWeb = async (req = request, res = response) => {
-	const { Servicio = "" } = req.query;
+	const { servicio:query = "" } = req.query;
 	const { page, limit } = req.query;
 	try {
 		const { skip, limite } = await evaluarPagina(page, limit);
 		let allServicios = await prisma.servicio.findMany({
 			where: {
 				Nombre: {
-					startsWith: Servicio,
+					startsWith: query,
 				},
 			},
 			skip,
@@ -55,32 +133,34 @@ const busquedaWeb = async (req = request, res = response) => {
 		});
 	}
 };
-const busquedaMobile = async (req = request, res = response) =>{
-	const { Servicio = "" } = req.query;
+const busquedaMobile = async (req = request, res = response) => {
+	const { Servicio:query = "" } = req.query;
 	const { page, limit } = req.query;
 	try {
 		const { skip, limite } = await evaluarPagina(page, limit);
 		let allServicios = await prisma.servicio.findMany({
 			where: {
 				Nombre: {
-					startsWith: Servicio,
+					startsWith: query,
 				},
 			},
-			select:{
-				Id:true,
+			select: {
+				Id: true,
 				Nombre: true,
-				Costo:true,
-				ImgPaths:true
+				Costo: true,
+				ImgPaths: true,
 			},
 			skip,
 			take: limite,
 		});
-		allServicios = getImgIdsFromService(allServicios)
-		return res.json(allServicios)
+		allServicios = getImgIdsFromService(allServicios);
+		return res.json(allServicios);
 	} catch (err) {
 		console.log(err);
 	}
-}
+};
 module.exports = {
-	busquedaGet,
+	busquedaServicios,
+	busquedaAlumnos,
+	busquedaTutores,
 };
--- routes/busqueda.js
+++ routes/busqueda.js
@@ -2,16 +2,29 @@ const { Router } = require('express')
 const { check } = require('express-validator')
 
 const { 
-    busquedaGet,
+    busquedaGet, busquedaServicios, busquedaAlumnos, busquedaTutores,
 } = require('../controllers/busqueda')
 const { validarPaginacion, validarCampos, validarJWT } = require('../middlewares')
+const { verifyAdminRole } = require('../middlewares/verifyRole')
 
 const router = Router()
-router.get('/',[
+router.get('/Servicios',[
     validarJWT,
     validarPaginacion, 
     validarCampos
-],busquedaGet )
+],busquedaServicios )
+router.get('/Alumnos',[
+    validarJWT,
+    verifyAdminRole,
+    validarPaginacion, 
+    validarCampos
+],busquedaAlumnos )
+router.get('/Tutores',[
+    validarJWT,
+    verifyAdminRole,
+    validarPaginacion, 
+    validarCampos
+],busquedaTutores )
 
 module.exports = router
     
